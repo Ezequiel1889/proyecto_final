f0b054373c30edf8169104e3cc69945f184b383a
Ezequiel Flammini <ezequiel.flammini@gmail.com>
2023-05-17 20:22:59 -0300

    trabaje con formulario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login as web_login
 from django.shortcuts import redirect
+
+from usuarios.forms import FormularioUsuario
 # Create your views here.
 
 
@@ -9,6 +11,7 @@ def login(request):
     
     if request.method == "POST":
         formulario = AuthenticationForm(request, data=request.POST)
+        
         
         if formulario.is_valid():
             nombre_usuario = formulario.cleaned_data.get("username")
@@ -21,5 +24,24 @@ def login(request):
     
     formulario = AuthenticationForm()
     return render(request, "usuarios/login.html", {"formulario":formulario})
+
+
+def registro(request):
+    
+    if request.method == "POST":
+        # formulario = UserCreationForm(request.POST)
+        formulario = FormularioUsuario(request.POST)
+        
+        if formulario.is_valid():
+            formulario.save()
+            return redirect("usuarios:login")
+        else:
+            return render(request, "usuarios/registro.html", {"formulario":formulario})
+            
+        
+    # formulario = UserCreationForm()
+    formulario = FormularioUsuario()
+    return render(request, "usuarios/registro.html", {"formulario":formulario})
+
 
 
